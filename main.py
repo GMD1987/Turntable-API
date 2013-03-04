f44f14112ad4b9f124bdf05ea9a3c8491edf58da
George DeGennaro (GMACHINE) <george.degennaro@gmail.com>
2013-03-04 03:23:25 -0500

    First commit, added main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from ttapi import Bot
+
+f = open('.ttc','r')
+
+for line in f:
+	config[line.split(1)] = line.split(3)
+
+bot = Bot(config[auth],config[userid])
+
+def roomChanged(data):
+	room = data['room']
+	metadata = room['metadata']
+	djcount = metadata['djcount']
+	if djcount < 4:
+		bot.addDj()
+	else:
+		bot.remDj()
+
+bot.on('roomChanged', roomChanged)
+
+
+bot.connect(config[roomid])
+bot.start
